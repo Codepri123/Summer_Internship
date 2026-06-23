b10b744036d9e046928518529b737ac497f5ca7c
Priyanka <priyanka@email.com>
2026-06-23 14:42:55 +0530

    add

--- Day_17/22_mini_project.py
+++ Day_17/22_mini_project.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
-students = ["A","B","C","D","E"]
-marks = [78,85,90,66,88]
-attendance = [90,85,95,80,92]
-plt.subplot(2,2,1)
-plt.title("Line plot")
-plt.plot(students,marks)
-plt.subplot(2,2,2)
-plt.title("bar plot")
-plt.bar(students,attendance)
-plt.subplot(2,2,3)
-plt.title("scatter plot")
-plt.scatter(students,marks)
-plt.subplot(2,2,4)
-plt.title("pie plot")
-plt.pie(marks,labels=students)
+
+students = ["A", "B", "C", "D", "E"]
+marks = [78, 85, 90, 66, 88]
+attendance = [90, 85, 95, 80, 92]
+
+plt.figure(figsize=(6, 6))  # Decrease overall graph size
+
+plt.subplot(2, 2, 1)
+plt.title("Line Plot")
+plt.plot(students, marks ,marker="o")
+plt.xlabel("Students")
+plt.ylabel("Marks")
+
+plt.subplot(2, 2, 2)
+plt.title("Bar Plot")
+plt.bar(students, attendance)
+plt.xlabel("Students")
+plt.ylabel("Attendance")
+
+plt.subplot(2, 2, 3)
+plt.title("Scatter Plot")
+plt.scatter(students, marks)
+plt.xlabel("Students")
+plt.ylabel("Marks")
+
+plt.subplot(2, 2, 4)
+plt.title("Pie Plot")
+plt.pie(marks, labels=students)
+
+plt.tight_layout()
 plt.show()
