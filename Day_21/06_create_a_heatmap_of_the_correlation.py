e68075a0356a866a7a45683e0d831c7fd0ae8fba
Priyanka <priyanka@email.com>
2026-06-29 14:53:39 +0530

    add

--- Day_21/06_create_a_heatmap_of_the_correlation.py
+++ Day_21/06_create_a_heatmap_of_the_correlation.py
@@ -1,17 +1,20 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Load dataset
 df = sns.load_dataset("tips")
-
+corr = df.corr(numeric_only=True)
+mask = np.tril(np.ones_like(corr, dtype=bool))
 print(df)
 
 # Heatmap
 sns.heatmap(
     df.corr(numeric_only=True),
-    cmap="Blues",
-    annot=True
+    cmap="coolwarm",
+    annot=True,
+    mask=mask
 )
 
 plt.show()
